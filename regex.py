d62505b017185649fdd766b0a7c5caa18e6535d2
ViniciusPetrattiCEC <viniciuspetratti@hotmail.com>
2023-03-23 13:54:29 -0300

    Função de pontuação funcionando
    Precissa corrigir os REGEX

--- regex.py
+++ regex.py
@@ -1,34 +1,27 @@
 import re
 
-regexes_dict = {
-    1 : ['0+1010+'],
-    2 : ['0+1{2}010+', '0+1{3}0+'],
-    3 : ['0+1{2}010+', '0+1{2}010+', '0+1{3}0+'],
-    4 : ['0{2}1{2}01{2}0{2}', '0{2}101{3}0{2}', '0{2}1{3}010{2}', '0{2}1{4}0{2}']
+regras = {
+    1: {'regex': ['0+10+'], 'pontos': 10},
+    2: {'regex': ['0+1{2}0+', '0+1010+'], 'pontos': 20},
+    3: {'regex': ['0+1{3}0+', '0+1{2}010+', '0+101{2}0+'], 'pontos': 30},
+    4: {'regex': ['0+1{4}0+', '0+1{3}010+', '0+101{3}0+', '0+1{2}01{2}0+'], 'pontos': 40}
 }
 
-regexes = [r'0+1010+', r'0+1{2}010+', r'0+1{3}0+',r'0+1{2}010+', r'0+1{2}010+', r'0+1{3}0+', 
-           r'0{2}1{2}01{2}0{2}', r'0{2}101{3}0{2}', r'0{2}1{3}010{2}', r'0{2}1{4}0{2}']
+
+def calcular_pontuacao(lista_de_strings, regras):
+    pontuacao_total = 0
+    for string in lista_de_strings:
+        for dados in regras.values():
+            for regex in dados['regex']:
+                if re.search(regex, string) is not None:
+                    pontuacao_total += dados['pontos']
+                    break
+    return pontuacao_total
+
+import matrizes
 
 
-import re
-
-def contar_correspondencias(strings, regexes_dict):
-    correspondencias = {}
-    for string in strings:
-        correspondencias[string] = {}
-        for chave, regexes in regexes_dict.items():
-            correspondencias[string][chave] = 0
-            for regex in regexes:
-                correspondencias[string][chave] += len(re.findall(regex, string))
-        if sum(correspondencias[string].values()) == 0:
-            del correspondencias[string]
-    return correspondencias
-
-
-import minimax, matrizes
-matches = contar_correspondencias(matrizes.obter_linhas_string(minimax.matrix), regexes_dict)
-print(matches)
+print(calcular_pontuacao(matrizes.obter_linhas_string(matrizes.matriz_teste), regras))
 
 
 
--- testes.py
+++ testes.py
@@ -1,35 +1,52 @@
 import re
+
+regras = {
+    1: {'regex': ['0+10+'], 'pontos': 10},
+    2: {'regex': ['0+1{2}0+', '0+1010+'], 'pontos': 20},
+    3: {'regex': ['0+1{3}0+', '0+1{2}010+', '0+101{2}0+'], 'pontos': 30},
+    4: {'regex': ['0+1{4}0+', '0+1{3}010+', '0+101{3}0+', '0+1{2}01{2}0+'], 'pontos': 40}
+}
+
+caso = ['0+1{3}0+', '0*1{5}0*']
+string = ["001110101110000"]
+
+regras_teste = {
+    1: {'regex': ['0+1{3}0+'], 'pontos': 30},
+}
+
+def calcular_pontuacao(lista_de_strings, regras):
+    pontuacao_total = 0
+    num_matches = []
+    for string in lista_de_strings:
+        passed = 0
+        for dados in regras.values():
+            for regex in dados['regex']:
+                matches = re.findall(regex, string)
+                passed += len(matches)
+                pontuacao_total += dados['pontos'] * passed
+        num_matches.append(passed)
+    return pontuacao_total, num_matches
+
+# def calcular_pontuacao(lista_de_strings, regras):
+#     pontuacao_total = 0
+#     for string in lista_de_strings:
+#         for dados in regras.values():
+#             for regex in dados['regex']:
+#                 if re.search(regex, string) is not None:
+#                     pontuacao_total += dados['pontos']
+#                     break
+#     return pontuacao_total
+
+
 import matrizes
-import minimax
+import pandas as pd
 
-regex1 = re.compile(r'0+1{4}0+')
-regex2= re.compile(r'0+1{3}010+')
-regex3 = re.compile(r'0+101{3}0+')
-regex4 = re.compile(r'0+1{2}01{2}0+')
+strings = matrizes.obter_linhas_string(matrizes.matriz_teste)
+pontuacao, num_matches = calcular_pontuacao(strings, regras)
 
-regex_vencedor = re.compile(r'1{5}')
-
-valores =["00111100", "001110100", "001011100", "001101100"]
-
-regexes = [regex1, regex2, regex3, regex4]
-
-tudo = matrizes.obter_linhas_string(minimax.matrix)
-
-# resultado = []
-# for regex in regexes:
-#     for valor in tudo:
-#         res = re.findall(regex, valor)
-#         if res != []:
-#             resultado.append(res)
-
-# for item in tudo:
-#     print(re.findall(regex_vencedor, item))
-
-# r_vencedor = re.compile(r'1{5}')
-
-for item in tudo:
-    if re.search("11111", item):
-        print("achei")
-        break
-
-
+for item in strings:
+    if re.fullmatch('0+', item) is None:
+        print("------------------")
+        print(item)
+        print(num_matches[strings.index(item)])
+        print("------------------")
